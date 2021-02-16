a8b5da6ff5718ac10c07ae12eda18d4c103f2417
Roye Abelman:q <royeabelman@gmail.com>
2021-02-16 09:40:32 +0200

    Small formatting changes

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ import zoo.exceptions.InvalidAnimalName;
 
 public class Main {
     public static final String NAME_SPLIT_BY_SPACE = " ";
-    public static final boolean STOP_PRINTING_WHEN_INVALID_NAME = false;
     private final static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
@@ -18,33 +17,29 @@ public class Main {
         List<String> animalNames;
         try {
             animalNames = FileUtilities.getWordsBySplitCondition(animalNameFilePath, NAME_SPLIT_BY_SPACE);
+            printAllAnimalsDetails(animalNames);
         }
         catch (FileNotFoundException fileNotFoundException) {
             logger.log(Level.FATAL, fileNotFoundException.getMessage(), fileNotFoundException);
-            return;
         }
-
-        printAllAnimalsDetails(animalNames);
     }
 
     public static void printAllAnimalsDetails(List<String> animalNames) {
         AnimalFactory animalFactory = new AnimalFactory();
         for (String animalName : animalNames) {
-            try {
-                printAnimalDetails(animalName, animalFactory);
-            }
-            catch (InvalidAnimalName invalidAnimalName) {
-                    logger.log(Level.WARN, invalidAnimalName.getMessage(), invalidAnimalName);
-                    if (STOP_PRINTING_WHEN_INVALID_NAME) {
-                        return;
-                    }
-            }
+            printAnimalDetails(animalName, animalFactory);
         }
     }
 
-    private static void printAnimalDetails(String animalName, AnimalFactory animalFactory) throws InvalidAnimalName{
+    private static void printAnimalDetails(String animalName, AnimalFactory animalFactory) {
+        try {
             Animal generatedAnimal = animalFactory.generateAnimal(animalName);
             generatedAnimal.printSound();
             generatedAnimal.printName();
+        }
+
+        catch (InvalidAnimalName invalidAnimalName) {
+            logger.log(Level.WARN, invalidAnimalName.getMessage(), invalidAnimalName);
+        }
     }
 }
--- src/zoo/exceptions/ZooException.java
+++ src/zoo/exceptions/ZooException.java
@@ -1,14 +1,7 @@
 package zoo.exceptions;
 
 public class ZooException extends Exception {
-    public ZooException(){
-        super();
-    }
     public ZooException(String message) {
         super(message);
-    }
-
-    public ZooException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
